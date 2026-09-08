f164bc65ba0265787a98069584a4d5b6884a6bb2
Munal Bhattarai <emailaddress9810@gmail.com>
2026-09-08 12:44:00 +0545

    updated the urls for trip app

--- backend/apps/trips/urls.py
+++ backend/apps/trips/urls.py
@@ -1,15 +1,37 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import TripViewSet, ItineraryViewSet
+from .views import (
+    AcceptMembershipView,
+    RejectMembershipView,
+    ItineraryViewSet,
+    TripViewSet,
+)
 
 
 router = DefaultRouter()
 router.register("trips", TripViewSet, basename="trip")
 
 trip_router = DefaultRouter()
-trip_router.register("itineraries", ItineraryViewSet, basename="trip-itinerary")
+trip_router.register(
+    "itineraries",
+    ItineraryViewSet,
+    basename="trip-itinerary",
+)
 
 urlpatterns = router.urls + [
-    path("trips/<int:trip_pk>/", include(trip_router.urls)),
+    path(
+        "trips/<int:trip_pk>/",
+        include(trip_router.urls),
+    ),
+    path(
+        "memberships/<int:membership_id>/accept/",
+        AcceptMembershipView.as_view(),
+        name="accept-membership",
+    ),
+    path(
+    "memberships/<int:membership_id>/reject/",
+    RejectMembershipView.as_view(),
+    name="reject-membership",
+),
 ]
