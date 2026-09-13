4914df54c4ff0afc63806f5e61ee8fb5ce78a1a8
Munal Bhattarai <emailaddress9810@gmail.com>
2026-09-13 11:45:00 +0545

    registered matching routes in project urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('api/v1/', include("apps.destinations.urls")),
     path('api/v1/', include("apps.trips.urls")),
     path('api/v1/', include("apps.reviews.urls")),
-    
+    path('api/v1/', include("apps.matching.urls")),
 ]
